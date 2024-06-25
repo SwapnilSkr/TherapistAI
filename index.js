2c10001296cc0bd225123adc2864ac3640a49aa8
Swapnil12022002 <swapnilmkab@gmail.com>
2024-06-25 17:37:48 +0530

    AI content

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.post("/callback", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => {
       const response = aiResponse();
-      console.log(response.choices[0].message.content);
-      return res.json(response.choices[0].message.content);
+      console.log(response?.choices[0]?.message?.content);
+      return res.json(response?.choices[0]?.message?.content);
     })
     .catch((err) => {
       console.error(err);
